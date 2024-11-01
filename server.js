1d0b701e9fd1ffd373c22411aea2e65f8d1b5db7
kvineet002 <134228421+kvineet002@users.noreply.github.com>
2024-11-02 03:26:15 +0530

    Update CORS configuration to allow requests from production URL

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000", // Replace with the URL of your React app
+    origin: ["http://localhost:3000","https://video-chat-self.vercel.app"], // Replace with the URL of your React app
     methods: ["GET", "POST"]
   }
 });
